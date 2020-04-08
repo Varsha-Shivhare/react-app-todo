8e28b4e2d591f94cbad9297116e89636b88351fb
Varsha-Shivhare <50067937+Varsha-Shivhare@users.noreply.github.com>
2020-04-08 12:17:49 +0530

    code added

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Todo from './Person/Todo';
+
+function App() {
+  return (
+    <div className="App-header">
+      <h3>Programs</h3>
+      <Todo />
+    </div>
+  );
+}
+
+export default App;
