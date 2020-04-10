1ec3cc5bee6c280a16c85f4b669e6480b0e50441
Varsha-Shivhare <50067937+Varsha-Shivhare@users.noreply.github.com>
2020-04-10 20:14:29 +0530

    new files

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
 import React from 'react';
 import './App.css';
 import Todo from './Person/Todo';
-import ToDoItem from './ToDoApp/ToDoList';
-import ToDoList from './ToDoApp/ToDoItem';
+
 
 function App() {
   return (
     <div className="App-header">
       <h3>Programs</h3>
       <Todo />
-      <ToDoItem />
-      <ToDoList />
     </div>
   );
 }
