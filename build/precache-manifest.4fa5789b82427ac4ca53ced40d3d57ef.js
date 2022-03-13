2571ef16f2198b821650f91c197b7e09c1c758af
Varsha-Shivhare <50067937+Varsha-Shivhare@users.noreply.github.com>
2022-03-13 19:21:53 +0530

    code added.

---
+++ build/precache-manifest.4fa5789b82427ac4ca53ced40d3d57ef.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e1f6429f4ed9340b4eb4",
+    "url": "/static/css/main.a8ed26ad.chunk.css"
+  },
+  {
+    "revision": "e1f6429f4ed9340b4eb4",
+    "url": "/static/js/main.018e6adc.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "1868546b76755d13629b",
+    "url": "/static/js/2.cdefec29.chunk.js"
+  },
+  {
+    "revision": "ae93a5197bec35424a6aacb0df9717ac",
+    "url": "/index.html"
+  }
+];
